38e958d5817ea3c6ae295d97b8cdad884214c129
ik_2dw3 <ik_2dw3@H015110.NET>
2019-10-15 14:23:13 +0200

    css en registro

--- php/registro.php
+++ php/registro.php
@@ -13,8 +13,11 @@ $pss = $_POST["contraseña"];
     a nuestra disposición. Por lo que podemos acceder a ellas tal como si hubiéramos
     copiado y pegado el código
 */
+
+//insertamos datos en la BD cread               nombre de table          nombre columnas
 $sentencia = $conexionBD->prepare("INSERT INTO erabiltzailea(izena, abizena, erabiltzaile_iz, email, pasahitza) VALUES (?, ?, ?, ?, ?);");
-$resultado = $sentencia->execute([$nombre, $apellido, $nombre_usuario, $correo, $pss]); # Pasar en el mismo orden de los ?
+$resultado = $sentencia->execute([$nombre, $apellido, $nombre_usuario, $correo, $pss]); 
+# Pasar en el mismo orden de los ?
 #execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
 #Con eso podemos evaluar
 if($resultado === TRUE) echo "Insertado correctamente $nombre y $apellido";
