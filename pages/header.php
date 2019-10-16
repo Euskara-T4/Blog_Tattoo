a54c50401b953a0d1a90eb9a7f191a2f454cc807
Nahia Maguregi <nahiamag19@gmail.com>
2019-10-16 12:41:50 +0200

    Añadir login y about us

---
+++ pages/header.php
@@ -0,0 +1,76 @@
+<?php
+    echo "          
+        <head>
+            <title>Blog Tattoo</title>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+            <link href='../layout/styles/layout.css' rel='stylesheet' type='text/css' media='all'>
+        </head>
+
+        <body id='top'>
+            <!-- ################################################################################################ -->
+            <div class='wrapper row0'>
+                <div id='topbar' class='hoc clear'>
+                    <ul>
+                        <li><i class='fa fa-clock-o'></i> Mon. - Fri. 8am - 5pm</li>
+                        <li><i class='fa fa-phone'></i> +00 (123) 456 7890</li>
+                        <li><i class='fa fa-envelope-o'></i> info@domain.com</li>
+
+                        <!-- BOTONES LOGIN -->
+                        <li><a href='#'><i class='fa fa-lg fa-home'></i></a></li>
+                        <li><a href='#' title='Login'><i class='fa fa-lg fa-sign-in'></i></a></li>
+                        <li><a href='#' title='Sign Up'><i class='fa fa-lg fa-edit'></i></a></li>
+                        <!-------------------------------------------->
+                    </ul>
+                </div>
+            </div>
+
+            <!-- ################################################################################################ -->
+            <!-- ################################################################################################ -->
+
+            <!-- Top Background Image Wrapper -->
+                <div class='wrapper row1'>
+
+                    <header id='header' class='hoc clear'>
+                        <div id='logo' class='fl_left'>
+                            <a href='../index.php'>
+                                <img src='../images/demo/logo3Txiki.jpg' alt='Logo blog' id='logoImg'>
+                            </a>
+                            <h1><a href='../index.php'>Blog tattoo</a></h1>
+                        </div>
+
+                        <nav id='mainav' class='fl_right'>
+                            <ul class='clear'>
+                                <li class='active'><a href='../blog/index.php'>Home</a></li>
+                                <li><a class='drop' href='#'>Pages</a>
+                                    <ul>
+                                        <li><a href='gallery.php'>Gallery</a></li>
+                                        <li><a href='full-width.html'>Full Width</a></li>
+                                        <li><a href='pages/sidebar-left.php'>Sidebar Left</a></li>
+                                        <li><a href='pages/sidebar-right.php'>Sidebar Right</a></li>
+                                        <li><a href='pages/basic-grid.php'>Basic Grid</a></li>
+                                    </ul>
+                                </li>
+
+                                <li><a class='drop' href='#'>Dropdown</a>
+                                    <ul>
+                                        <li><a href='#'>Level 2</a></li>
+                                        <li><a class='drop' href='#'>Level 2 + Drop</a>
+                                            <ul>
+                                                <li><a href='#'>Level 3</a></li>
+                                                <li><a href='#'>Level 3</a></li>
+                                                <li><a href='#'>Level 3</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href='#'>Level 2</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href='#'>Link Text</a></li>
+                                <li><a href='#'>Link Text</a></li>
+                            </ul>
+                        </nav>
+                    </header>
+                </div>
+        ";
+
+?>
